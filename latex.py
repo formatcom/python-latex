088abc3a5898fa70c9fdd79212a2e7f8162d7201
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2021-04-12 14:58:02 +0200

    fix support github

--- latex.py
+++ latex.py
@@ -10,10 +10,14 @@ from matplotlib.font_manager import FontProperties
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--input',       required=True)
+parser.add_argument('--output',      default='latex.svg')
+parser.add_argument('--name',        default='latex')
 parser.add_argument('--family',      default='serif')
 parser.add_argument('--mathfamily',  default='dejavuserif')
 parser.add_argument('--style',       default='italic')
 parser.add_argument('--size',        default=16, type=int)
+parser.add_argument('--base64',      action='store_true')
+parser.add_argument('--markdown',    action='store_true')
 args = parser.parse_args()
 
 font = FontProperties(family=args.family,
@@ -27,8 +31,21 @@ mathtext.math_to_image(r'{}'.format(args.input), buffer,
 
 buffer.seek(0)
 
-encoded = 'data:image/svg+xml;;base64,{}'.format(
-                        base64.b64encode(buffer.read()).decode('utf-8'))
+if args.base64:
+    encoded = 'data:image/svg+xml;base64,{}'.format(
+                            base64.b64encode(buffer.read()).decode('utf-8'))
 
+    if args.markdown:
+        print('base64 and markdown no support', file=sys.stderr)
+    else:
+        print(encoded, end='')
 
-print(encoded, end='')
+else:
+
+    f = open(args.output, 'wb')
+    f.write(buffer.read())
+    f.close()
+
+    if args.markdown:
+        print('![{name}]({output})'.format(name=args.name, output=args.output), end='')
+
